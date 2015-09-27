ad9b49d5b63f5a233ec38c65be8b19e4087e3cdc
Mann <chanmannlim@gmail.com>
2015-09-27 12:04:59 -0500

    evaluate linear regression model

--- 3/problem_1.m
+++ 3/problem_1.m
@@ -6,16 +6,19 @@ y = [52.21 53.12 54.48 55.84 57.2 58.57 59.93 61.29 63.11 64.47 66.28 68.1 69.92
 A = [ones(length(x), 1) x];
 beta = (A'*A)\A'*y;
 
+% Evaluation
+prediction = A*beta;
+expected_loss_beta = (y-prediction)' * (y-prediction) / length(y);
+display(expected_loss_beta);
+
 % Polynomial regression
 A = [A x.^2];
 beta_2 = (A'*A)\A'*y;
 
 % Evaluation
 prediction = A*beta_2;
-expected_loss = (y-prediction)' * (y-prediction) / length(y);
-
-display(prediction);
-display(expected_loss);
+expected_loss_beta2 = (y-prediction)' * (y-prediction) / length(y);
+display(expected_loss_beta2);
 
 % Plot
 figure;
